3b357a93b0420751738c4e5041b8e47bb738c63a
jongseo98 <jspark98@unist.ac.kr>
2022-09-02 00:25:41 +0900

    4.4 전체 조회 화면까지

---
+++ src/main/java/com/jspark98/springbootaws/web/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.jspark98.springbootaws.web;
+
+import com.jspark98.springbootaws.service.posts.PostsService;
+import com.jspark98.springbootaws.web.dto.PostsResponseDto;
+import com.jspark98.springbootaws.web.dto.PostsSaveRequestDto;
+import com.jspark98.springbootaws.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
---
+++ src/main/java/com/jspark98/springbootaws/web/dto/PostsResponseDto.java
@@ -0,0 +1,19 @@
+package com.jspark98.springbootaws.web.dto;
+
+import com.jspark98.springbootaws.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
