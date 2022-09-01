b177c350e5247e8fea02136b0bedd7e0569c7062
jongseo98 <jspark98@unist.ac.kr>
2022-09-02 04:05:01 +0900

    google login success

--- src/main/java/com/jspark98/springbootaws/web/IndexController.java
+++ src/main/java/com/jspark98/springbootaws/web/IndexController.java
@@ -1,25 +1,41 @@
 package com.jspark98.springbootaws.web;
 
+import com.jspark98.springbootaws.config.auth.dto.SessionUser;
 import com.jspark98.springbootaws.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+           model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        model.addAttribute("post", postsService.findById(id));
+        return "posts-update";
     }
 }
