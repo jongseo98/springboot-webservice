f9f39c820cd5388cb51940dc4dcc9b52cb70471d
jongseo98 <jspark98@unist.ac.kr>
2022-09-01 21:03:28 +0900

    초기 설정

---
+++ src/test/java/com/jspark98/springbootaws/SpringbootAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jspark98.springbootaws;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
