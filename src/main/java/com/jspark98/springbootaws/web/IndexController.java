1318e4b300179935ac5b09b4bfc121781829f4b9
jongseo98 <jspark98@unist.ac.kr>
2022-09-02 17:43:51 +0900

    Session User 를 어노테이션 기반으로 개선

--- src/main/java/com/jspark98/springbootaws/web/IndexController.java
+++ src/main/java/com/jspark98/springbootaws/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jspark98.springbootaws.web;
 
+import com.jspark98.springbootaws.config.auth.LoginUser;
 import com.jspark98.springbootaws.config.auth.dto.SessionUser;
 import com.jspark98.springbootaws.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
@@ -18,9 +19,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
            model.addAttribute("userName", user.getName());
         }
